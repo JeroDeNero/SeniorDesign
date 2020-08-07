02f9793d2972e62b88fee610457bc6471c1e237a
CallumCoy <gamecc@ymail.com>
2020-08-07 14:59:27 -0400

    added all the clicking functions, along with the todo for each function.

--- site/src/app/controls/controls.component.ts
+++ site/src/app/controls/controls.component.ts
@@ -5,11 +5,46 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './controls.component.html',
   styleUrls: ['./controls.component.css']
 })
+
+
 export class ControlsComponent implements OnInit {
 
-  constructor() { }
+  movementState: Ihash;
 
-  ngOnInit(): void {
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.movementState["forward"] = false;
+    this.movementState["backward"] = false;
+    this.movementState["right"] = false;
+    this.movementState["left"] = false;
+  }
+
+  onClick(button) {
+    // TODO call server, if the erver could return set speed that would be perfect.
+  }
+
+  onClickD(button) {
+    //TODO contact server with button (if the server could return true on sucessfully recieving info)
+    this.movementState[button] = true;
+  }
+
+  onClickR(button) {
+    if (this.movementState[button]) {
+      //TODO contact server with button
+      this.movementState[button] = false;
+    }
+  }
+
+  newRun() {
+
+    window.alert('new run');
+    //TODO will pull up the form asking for the details about the details
+  }
+
+}
+
+interface Ihash {
+  [key: string]: boolean;
 }
