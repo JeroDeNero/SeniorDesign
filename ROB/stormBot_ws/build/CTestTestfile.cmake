b96a69ef55ed791884f59febc3bbe928a8e53d01
Robert Lee <rlee373@gmail.com>
2020-09-14 15:02:13 -0400

    initial workspace and movement package set up

---
+++ ROB/stormBot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rob/Desktop/repo/SeniorDesign/ROB/stormBot_ws/src
+# Build directory: /home/rob/Desktop/repo/SeniorDesign/ROB/stormBot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("movement")
---
+++ ROB/stormBot_ws/build/movement/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rob/Desktop/repo/SeniorDesign/ROB/stormBot_ws/src/movement
+# Build directory: /home/rob/Desktop/repo/SeniorDesign/ROB/stormBot_ws/build/movement
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
