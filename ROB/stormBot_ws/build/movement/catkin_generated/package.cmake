0472a3f41b9e4811298410e5c3d7e220924e6955
Robert Lee <rlee373@gmail.com>
2020-09-21 00:06:00 -0400

    move specific distance publisher

--- ROB/stormBot_ws/build/movement/catkin_generated/package.cmake
+++ ROB/stormBot_ws/build/movement/catkin_generated/package.cmake
@@ -1,6 +1,6 @@
 set(_CATKIN_CURRENT_PACKAGE "movement")
 set(movement_VERSION "0.0.0")
-set(movement_MAINTAINER "rob <rob@todo.todo>")
+set(movement_MAINTAINER "Robert Lee <rlee373@knights.ucf.edu>")
 set(movement_PACKAGE_FORMAT "2")
 set(movement_BUILD_DEPENDS "roscpp" "rospy" "std_msgs")
 set(movement_BUILD_EXPORT_DEPENDS "roscpp" "rospy" "std_msgs")
---
+++ ROB/stormBot_ws/build/ros_essentials_cpp/CMakeFiles/open_copy_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/open_copy_cpp.dir/src/topic03_perception/cpp/open_copy.cpp.o"
+  "/home/rob/Desktop/repo/SeniorDesign/ROB/stormBot_ws/devel/lib/ros_essentials_cpp/open_copy_cpp.pdb"
+  "/home/rob/Desktop/repo/SeniorDesign/ROB/stormBot_ws/devel/lib/ros_essentials_cpp/open_copy_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/open_copy_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
