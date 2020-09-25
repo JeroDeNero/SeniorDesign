c6fb7a00a6367e1ce92b98fc197f7ac28085f5b9
CallumCoy <gamecc@ymail.com>
2020-09-25 11:08:34 -0400

    toggle updates
    New variables to control when different things are shown.

--- site/frontend/src/app/toggle.service.ts
+++ site/frontend/src/app/toggle.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, ReplaySubject } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { RunService } from './run.service';
 
 @Injectable({
@@ -8,6 +9,12 @@ import { RunService } from './run.service';
 export class ToggleService {
   hideNew: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   hideEdit: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  showFavorites: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  showNamed: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  showUnamed: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+
+  resetFilter: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
 
   buttonOp: BehaviorSubject<string> = new BehaviorSubject<string>('New Run');
 
@@ -26,6 +33,17 @@ export class ToggleService {
       this.runService.addRun().subscribe();
       this.setButtonOp('New Run');
     }
+  }
+
+  toggleResetFilter() {
+    this.resetFilter.next(!this.resetFilter.getValue());
+    setTimeout(() => {
+      this.resetFilter.next(!this.resetFilter.getValue());
+    }, 1);
+  }
+
+  getResetFilter() {
+    return this.resetFilter.asObservable();
   }
 
   toggleHideNew(): void {
@@ -54,5 +72,29 @@ export class ToggleService {
 
   getButtonOp(): Observable<string> {
     return this.buttonOp.asObservable();
+  }
+
+  setShowFavorites(value) {
+    this.showFavorites.next(value);
+  }
+
+  getShowFavorites() {
+    return this.showFavorites.asObservable();
+  }
+
+  setShowNamed(value) {
+    this.showNamed.next(value);
+  }
+
+  getShowNamed() {
+    return this.showNamed.asObservable();
+  }
+
+  setShowUnamed(value) {
+    this.showUnamed.next(value);
+  }
+
+  getShowUnamed() {
+    return this.showUnamed.asObservable();
   }
 }
