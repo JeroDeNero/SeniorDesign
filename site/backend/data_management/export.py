1a7be00a5bc2b8199bc43cb948156d8c3bb290a1
Jero <jero96@knights.ucf.edu>
2020-09-29 18:53:02 -0400

    check plz

--- site/backend/data_management/export.py
+++ site/backend/data_management/export.py
@@ -8,38 +8,37 @@ from data_management.get import getOldest, getRun, getTag, getVideo
 from data_management.delete import garbageCollector
 
 from flask import(
-    Blueprint, request, jsonify
+    Blueprint, request, jsonify, send_file
 )
 
 bp = Blueprint('export', __name__, url_prefix='/export')
 
 
-@bp.route('/tag', methods=(['POST']))
+@bp.route('/locationExport', methods=(['POST']))
 def exportTag():
 
     db = getDb() # access the DB
-    rid = getRun() # obtain the run ID i.e. PipeID
-    tid = getTag(db, rid) # use to get tag ID for coords
-    video = getVideo(db, rid) # get the video info i.e. date
-    path = #what is the path?
+    runID = getRun() # obtain the run ID i.e. PipeID
+    tagData = getTag(db, runID) # get coords
+    video = getVideo(db, runID) # get the video info i.e. date
 
-    theDate = video[5] # retrieve the date
-    longitude = tid[1] # retrieve the x coordinate
-    latitude = tid[2] # retrieve the y coordinate
+    theDate = video.get("DateTaken") # retrieve the date
+    longitude = tagData.get("Longi") # retrieve the x coordinate
+    latitude = tagData.get("Lat") # retrieve the y coordinate
 
-    sfw = shapefile.Writer(path, shapeType = shapefile.POINT) #writing a new shapefile
-    sfw.autobalance = True #alternatively can be set to 1 for true
+    sfw = shapefile.Writer('TEMP/tmp_coord.shp', shapeType = shapefile.POINT) # writing a new shapefile
+    sfw.autobalance = True # alternatively can be set to 1 for true
 
-    #setting up the fields for writing the data to the shapefile
-    sfw.field('xcord', 'N') #xcoordinate
-    sfw.field('ycord', 'N') #ycoordinate
-    sfw.field('Date', 'D') #input the date
+    # setting up the fields for writing the data to the shapefile
+    sfw.field('xcord', 'N') # xcoordinate
+    sfw.field('ycord', 'N') # ycoordinate
+    sfw.field('Date', 'D') # input the date
 
-    #write the shapefile
+    # write the shapefile
     sfw.point(longitude, latitude)
     sfw.record(longitude, latitude, theDate)
 
     sfw.close()
     db.close()
 
-    return jsonify({})
+    return send_file('/TEMP/tmp_coord.shp', attachment_filename = 'coord.shp')
