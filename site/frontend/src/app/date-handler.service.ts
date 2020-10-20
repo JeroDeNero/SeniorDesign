91ccf3060bc2121c651871b588dc57e2d8a4ef4c
Callum Coy <callummichaelcoy242@gmail.com>
2020-10-20 17:33:16 -0400

    date handler
    takes an inputed date and parses it to the date format that is used for saving a runs information.

---
+++ site/frontend/src/app/date-handler.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DateHandlerService {
+
+  constructor() { }
+
+  private DaysEnum = Object.freeze({
+    "jan":'01',
+    "feb":'02', 
+    "mar":'03', 
+    "apr":'04', 
+    "may":'05', 
+    "june":'06', 
+    "july":'07', 
+    "aug":'08', 
+    "sept":'09', 
+    "oct":'10', 
+    "nov":'11', 
+    "dec":'12' })
+
+  generateDate(fullDate : Date) : String{
+
+    const date = String(fullDate).match(/\d\d.[a-zA-Z]+.\d\d\d\d.\d\d:\d\d:\d\d/)[0];
+
+    const data = date.split(' ');
+    const day = data[0];
+    const month = this.DaysEnum[data[1].toLocaleLowerCase()];
+    const year = data[2];
+    const time =  data[3];
+
+    return `${year}-${month}-${day}_${time}`
+  }
+}
