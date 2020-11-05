c8da535b840442e43dda683c908a490e07b3e81d
Robert Lee <rlee373@gmail.com>
2020-11-04 23:11:33 -0500

    teleop sending highLow forwardBack stop

--- ROB/dev_ws/build/teleop_twist_keyboard/build/lib/teleop_subscriber.py
+++ ROB/dev_ws/build/teleop_twist_keyboard/build/lib/teleop_subscriber.py
@@ -1,6 +1,45 @@
+from __future__ import division
+from sys import exit
+from signal import signal, SIGINT
+import time
+
+# test.py above
 from geometry_msgs.msg import Twist
 import rclpy
 from rclpy.node import Node
+
+# DIR = motor1, DIR2 = motor2, GPIO.LOW = forward, GPIO.HIGH = backward
+def testMotors(speed):
+    print('speed: %s' % speed)
+    # for turning, will need to expand params to (leftSpeed, rightSpeed)
+    # ex. turn left, leftSpeed = 0 or stopped, rightSpeed = 0.5 or low
+    # pwm1.set_pwm(0,2000,0) = left motor low
+    # pwm1.set_pwm(1, 4096, 0) = right motor high
+    
+    #while True:
+        #timer = 5
+        # GPIO.output(DIR, GPIO.LOW)
+        # GPIO.output(DIR2, GPIO.LOW)
+        # pwm1.set_pwm(0,2000,0)
+        # pwm1.set_pwm(1,2000,0)
+        #time.sleep(timer)
+        #print('DIR changed1')
+        # GPIO.output(DIR, GPIO.HIGH)
+        # GPIO.output(DIR2, GPIO.HIGH)
+        #time.sleep(timer)
+        #print('Power to Max')
+        # pwm1.set_pwm(0, 4096, 0)
+        # pwm1.set_pwm(1, 4096, 0)
+        #time.sleep(timer)
+        #print('DIR changed2')
+        # GPIO.output(DIR, GPIO.LOW)
+        # GPIO.output(DIR2, GPIO.LOW)
+        #time.sleep(timer)
+        #print('Stopping')
+        # pwm1.set_pwm(0,0,0)
+        # pwm1.set_pwm(1,0,0)
+        #time.sleep(timer)
+        # signal(SIGINT, handler)
 
 class KeyboardSubscriber(Node):
 
@@ -14,9 +53,29 @@ class KeyboardSubscriber(Node):
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        self.get_logger().info("receiving keyboard commands: ")
+        # self.get_logger().info("receiving keyboard commands: ")
         self.get_logger().info("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
         self.get_logger().info("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
+
+        targ = 35
+        DIR = 40
+        DIR2 = 38
+        safeNumber = 7
+
+        if msg.linear.x > 0 and msg.linear.x <= 0.5:
+            speed = 'low_forward'
+        elif msg.linear.x > 0.5:
+            speed = 'high_forward'
+        elif msg.linear.x < 0 and msg.linear.x >= -0.5:
+            speed = 'low_backward'
+        elif msg.linear.x < -0.5:
+            speed = 'high_backward'
+        else:
+            speed = 'stopped'
+
+        testMotors(speed)
+        time.sleep(1)
+        #signal(SIGINT, handler)
 
 def main(args=None):
     rclpy.init(args=args)
